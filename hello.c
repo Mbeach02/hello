d96bbddc6e25974640ed8ac72adc28e99ce7936f
Maclain Beach <mbeach4@huskers.unl.edu>
2021-08-26 22:30:22 +0000

    added my major to the output

---
+++ hello.c
@@ -0,0 +1,16 @@
+/**
+ * Author: Maclain Beach
+ * 2021/08/25
+ *
+ * A simplpe hello world program in C
+ *
+ * */
+    #include <stdlib.h>
+    #include <stdio.h>
+    int main(int argc, char **argv) {
+
+   printf("Maclain Beach!\n");
+   printf("Computer Engineering\n");
+
+   return 0;
+ }
